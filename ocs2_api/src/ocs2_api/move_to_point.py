e98c76f206555167ef4179a1f9ec47f2e61525ac
KortexXD <oborny.to@gmail.com>
2024-01-21 02:10:17 +0100

    fixed moving to desired point

--- ocs2_api/src/ocs2_api/move_to_point.py
+++ ocs2_api/src/ocs2_api/move_to_point.py
@@ -5,6 +5,7 @@ from geometry_msgs.msg import PoseStamped
 from gazebo_msgs.msg import ModelStates
 from geometry_msgs.msg import Twist
 import numpy as np
+import tf
 
 class MoveToPoint:
     def __init__(self):
@@ -14,17 +15,21 @@ class MoveToPoint:
         self.curr_position_y = 0
         self.curr_orientation = 0
         self.got_command2 = False
-        #rospy.init_node('tu')
+        self.rotate = True
     def new_state_callback(self,data):
         point = data.point
         self.new_x = point.x
         self.new_y = point.y
         self.got_command2 = True
+        self.rotate = True
     def curr_state_callback(self,data):
         pose = data.pose[-1]
         self.curr_position_x = pose.position.x
         self.curr_position_y = pose.position.y
-        self.curr_orientation = pose.orientation.z
+        self.curr_orientation_x = pose.orientation.x
+        self.curr_orientation_y = pose.orientation.y
+        self.curr_orientation_z = pose.orientation.z
+        self.curr_orientation_w = pose.orientation.w
 
     def new_request_callback(self,data):
         if data is not None:
@@ -43,71 +48,48 @@ class MoveToPoint:
         vector = T @ np.array([x, y])
         return vector
 
+    def new_orientation(self):
+        spot_to_point = [self.new_x - self.curr_position_x, self.new_y - self.curr_position_y]
+        orientation_to_point = np.arctan2(spot_to_point[1], spot_to_point[0]) # /np.pi
+        return spot_to_point, orientation_to_point
+
+    def euler_from_quaternion(self):
+        euler = tf.transformations.euler_from_quaternion([self.curr_orientation_x, self.curr_orientation_y,
+                                                         self.curr_orientation_z, self.curr_orientation_w])
+        return euler[-1]
 
     def move_to_point(self):
         twist_pub = rospy.Publisher('twist_cmd', Twist, queue_size=10)
         rate = rospy.Rate(10)
-        last_move = [0, 0, 0]
         while not rospy.is_shutdown():
-
             self.listener()
-            if not self.got_command2:
-                self.new_x = self.curr_position_x
-                self.new_y = self.curr_position_y
-                #self.got_command = True
-                #continue
-            new_orientation = np.arctan2(self.new_y-self.curr_position_y, self.new_x-self.curr_position_x) / np.pi
-            move = Twist()
-            curr_point = np.array([self.curr_position_x, self.curr_position_y])
-            end_point = np.array([self.new_x, self.new_y])
-            vector = end_point-curr_point
-
-            if np.linalg.norm(vector) > 1:
-                vector = self.coord_transformation(self.curr_orientation * np.pi, vector[0], vector[1])
-                vector = (vector/np.linalg.norm(vector))
-                move.linear.x = vector[0] * 0.3
-                move.linear.y = vector[1] * 0.2
-                if abs(new_orientation - self.curr_orientation) > 0.05:
-                    if new_orientation - self.curr_orientation > 0:
-                        move.angular.z = 0.15
-                    else:
-                        move.angular.z = - 0.15
-            else:
-                vector = self.coord_transformation(self.curr_orientation * np.pi, vector[0], vector[1])
-                if np.linalg.norm(vector) > 0.5:
-                    move.linear.x = vector[0] * 0.3 * np.linalg.norm(vector)
-                    move.linear.y = vector[1] * 0.2 * np.linalg.norm(vector)
-                elif np.linalg.norm(vector) > 0.2:
-                    move.linear.x = vector[0] * np.linalg.norm(vector)
-                    move.linear.y = vector[1] * np.linalg.norm(vector)
-                elif np.linalg.norm(vector) > 0.01:
-                    move.linear.x = vector[0]
-                    move.linear.y = vector[1]
-                else:
-                    self.got_command2 = False
-            """else:
-                if np.linalg.norm(vector) > 0.05:
-                    move.linear.x = vector[0] * 0.4 #* np.linalg.norm(vector)
-                    move.linear.y = vector[1] * 0.3 #* np.linalg.norm(vector)
-                else:
-                    self.got_command = False"""
-
             if self.got_command2:
-                #print("publishing")
+                move = Twist()
+                spot_to_point, orientation_to_point = self.new_orientation()
+                norm = np.linalg.norm(spot_to_point)
+                current_angle = self.euler_from_quaternion()
+                vector = self.coord_transformation(current_angle, spot_to_point[0], spot_to_point[1])
+                if abs(orientation_to_point-current_angle) > 0.15:
+                    if orientation_to_point-current_angle > 0.3:
+                        move.angular.z = 0.3
+                    elif orientation_to_point-current_angle < -0.3:
+                        move.angular.z = -0.3
+                    else:
+                        move.angular.z = orientation_to_point-current_angle
+                else:
+                    self.rotate = False
+                if not self.rotate:
+                    if norm > 2:
+                        norm_vector = vector/norm
+                        move.linear.x = 0.5*norm_vector[0]
+                        move.linear.y = 0.3*norm_vector[1]
+                    elif norm > 1:
+                        move.linear.x = 0.3*norm_vector[0]
+                        move.linear.y = 0.2*norm_vector[1]
+                    elif norm > 0.05:
+                        move.linear.x = 0.15*vector[0]
+                        move.linear.y = 0.08*vector[1]
+                    else:
+                        self.got_command2 = False
                 twist_pub.publish(move)
             rate.sleep()
-#MoveToPoint().move_to_point()
-
-"""if __name__ == '__main__':
-    try:
-        rospy.init_node('move_to_point')
-        MoveToPoint().move_to_point()
-        rospy.spin()
-    except rospy.ROSInterruptException:
-        pass"""
-
-
-
-
-
-
