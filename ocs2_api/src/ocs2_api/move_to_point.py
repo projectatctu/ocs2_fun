adding obstacle detection in move_to_point, checking correct user input in height.py and tilt.py, unit conversion between input and publisher

ocs2_api/src/ocs2_api/move_to_point.py
@@ -6,6 +6,7 @@ from gazebo_msgs.msg import ModelStates
 from geometry_msgs.msg import Twist
 import numpy as np
 import tf
+from std_msgs.msg import Bool
 
 class MoveToPoint:
     def __init__(self):
@@ -16,6 +17,7 @@ class MoveToPoint:
         self.curr_orientation = 0
         self.got_command2 = False
         self.rotate = True
+        self.detect_obstacle = False
     def new_state_callback(self,data):
         point = data.point
         self.new_x = point.x
@@ -35,10 +37,15 @@ class MoveToPoint:
         if data is not None:
             self.got_command2 = False
 
+    def detect_obstacle_callback(self,data):
+        self.detect_obstacle = data.data
+
+
     def listener(self):
         rospy.Subscriber("/clicked_point", PointStamped, self.new_state_callback)
         rospy.Subscriber("/gazebo/model_states", ModelStates, self.curr_state_callback)
         rospy.Subscriber("/move_base_simple/goal", PoseStamped, self.new_request_callback)
+        rospy.Subscriber("/slow_down", Bool, self.detect_obstacle_callback)
 
     def coord_transformation(self,theta,x,y):
         T = np.array([
@@ -69,6 +76,9 @@ class MoveToPoint:
                 norm = np.linalg.norm(spot_to_point)
                 current_angle = self.euler_from_quaternion()
                 vector = self.coord_transformation(current_angle, spot_to_point[0], spot_to_point[1])
+                max_speed = 0.6
+                if self.detect_obstacle:
+                    max_speed /= 2
                 if abs(orientation_to_point-current_angle) > 0.15:
                     if orientation_to_point-current_angle > 0.3:
                         move.angular.z = 0.3
@@ -79,10 +89,10 @@ class MoveToPoint:
                 else:
                     self.rotate = False
                 if not self.rotate:
+                    norm_vector = vector / norm
                     if norm > 2:
-                        norm_vector = vector/norm
-                        move.linear.x = 0.5*norm_vector[0]
-                        move.linear.y = 0.3*norm_vector[1]
+                        move.linear.x = max_speed*norm_vector[0]
+                        move.linear.y = (max_speed/2)*norm_vector[1]
                     elif norm > 1:
                         move.linear.x = 0.3*norm_vector[0]
                         move.linear.y = 0.2*norm_vector[1]

ocs2_api/src/ocs2_api/tilt.py
@@ -1,30 +1,48 @@
 #!/usr/bin/env python
-import sys
-sys.path.append("/opt/ros/noetic/lib/python3/dist-packages")
-
 import rospy
-from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Float32
+import numpy as np
 
 def main():
-    tilt_pub = rospy.Publisher('/tilt', Float32MultiArray, queue_size=10)
+    tilt_pub = rospy.Publisher('/tilt', Float32, queue_size=10)
+    tilt_dir_pub = rospy.Publisher('/tilt_dir', Float32, queue_size=10)
     rospy.init_node('Tilt')
-    print("Input tilt -20 to 20 degrees:")
-    tilt = input()
-    print("Input tilt direction 0 to 360 degrees")
-    tilt_dir = input()
-    tilt = float(tilt)
-    tilt_dir = float(tilt_dir)
-    if tilt < -20:
-        tilt = -20
-    if tilt > 20:
-        tilt = 20
-    if tilt_dir < 0:
-        tilt_dir = 0
-    if tilt_dir > 360:
-        tilt_dir = 360
-    arr = Float32MultiArray()
-    arr.data = [tilt,tilt_dir]
-    tilt_pub.publish(arr)
+    tilt = user_input_tilt()
+    tilt_dir = user_input_tilt_direction()
+    tilt = max(-20.0, min(tilt, 20.0))
+    tilt = tilt*np.pi/180
+    tilt_dir = max(0.0, min(tilt_dir, 360.0))
+    tilt_dir = tilt_dir*np.pi/180
+    desired_tilt = Float32()
+    desired_tilt_direction = Float32()
+    desired_tilt.data = tilt
+    desired_tilt_direction.data = tilt_dir
+    tilt_pub.publish(desired_tilt)
+    tilt_dir_pub.publish(desired_tilt_direction)
+
+def user_input_tilt():
+    correct_input = False
+    while not correct_input:
+        try:
+            input_float = float(input("Enter tilt from -20 to 20 degrees: "))
+            correct_input = True
+        except ValueError:
+            print("Invalid input. Enter number between -20 and 20")
+    return input_float
+
+def user_input_tilt_direction():
+    correct_input = False
+    while not correct_input:
+        try:
+            input_float = float(input("Enter direction of tilt from 0 to 360 degrees: "))
+            correct_input = True
+        except ValueError:
+            print("Invalid input. Enter number between 0 to 360 degrees")
+    return input_float
+
+
+
+
 
 if __name__ == '__main__':
     main()